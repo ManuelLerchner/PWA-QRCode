2f929c58854878d8e5026ac52ef12df5a297dd79
Manuel Lerchner <manuel.lerchner1111@gmail.com>
2022-03-06 15:54:03 +0100

    qr code scanner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  qrResultString: string = '-';
+
+  clearResult(): void {
+    this.qrResultString = '-';
+  }
+
+  onCodeResult(resultString: string) {
+    this.qrResultString = resultString;
+    console.log(this.qrResultString);
+  }
   title = 'test';
 }
